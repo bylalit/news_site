9ae3c581ac879596379e566a6ae4dc51cc1d9251
Lalit0101 <lalitkp0101@gmail.com>
2024-09-09 22:10:51 +0530

    okk

--- news-site/admin/add-post.php
+++ news-site/admin/add-post.php
@@ -7,7 +7,7 @@
              </div>
               <div class="col-md-offset-3 col-md-6">
                   <!-- Form -->
-                  <form  action="" method="POST" enctype="multipart/form-data">
+                  <!-- <form  action="" method="POST" enctype="multipart/form-data">
                       <div class="form-group">
                           <label for="post_title">Title</label>
                           <input type="text" name="post_title" class="form-control" autocomplete="off" required>
@@ -27,7 +27,7 @@
                           <input type="file" name="fileToUpload" required>
                       </div>
                       <input type="submit" name="submit" class="btn btn-primary" value="Save" required />
-                  </form>
+                  </form> -->
                   <!--/Form -->
               </div>
           </div>
