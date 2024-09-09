7162e5cd6ec190b3b52dadc617c02ee7dac5a780
Lalit0101 <lalitkp0101@gmail.com>
2024-09-09 22:15:13 +0530

    done the

--- news-site/admin/post.php
+++ news-site/admin/post.php
@@ -1,4 +1,4 @@
-<?php include "header.php"; ?>
+<!-- <?php include "header.php"; ?>
   <div id="admin-content">
       <div class="container">
           <div class="row">
@@ -103,4 +103,4 @@
           </div>
       </div>
   </div>
-<?php include "footer.php"; ?>
+<?php include "footer.php"; ?> -->
