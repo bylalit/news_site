53dd59b6226b53831794d2a07a099570b70c6df0
Lalit0101 <lalitkp0101@gmail.com>
2024-09-10 18:53:39 +0530

    file copy

--- news-site/admin/post.php
+++ news-site/admin/post.php
@@ -9,6 +9,22 @@
                   <a class="add-new" href="add-post.php">add post</a>
               </div>
               <div class="col-md-12">
+              <?php
+                    include "config.php";
+                    $limit = 3;
+                    
+                    if(isset($_GET['page'])){
+                        $page = $_GET['page'];
+                    }else{
+                        $page = 1;
+                    }
+                    $offset = ($page - 1) * $limit;
+
+                    $sql = "SELECT * FROM user ORDER BY user_id DESC  LIMIT {$offset},{$limit}";
+                    $result = mysqli_query($conn, $sql) or die("Query Unsuccessful.");
+
+                    if(mysqli_num_rows(($result)) > 0){
+                ?>
                   <table class="content-table">
                       <thead>
                           <th>S.No.</th>
@@ -94,6 +110,43 @@
                           </tr>
                       </tbody>
                   </table>
+
+
+                  <?php
+                        }
+
+                        $sql1 = "SELECT * FROM user";
+                        $result1 = mysqli_query($conn, $sql1) or die("Query Failed");
+
+                        if(mysqli_num_rows($result1) > 0){
+                            $total_records = mysqli_num_rows($result1);
+                            
+                            $total_page = ceil($total_records / $limit);
+
+                        
+                            echo "<ul class='pagination admin-pagination'>";
+                            if($page > 1){
+                                echo  '<li><a href="users.php?page='.($page - 1).'">Prev</a></li>';
+                            }
+                            
+                            for($i = 1; $i <=  $total_page; $i++){
+                                if($i  == $page){
+                                    $active =  "active";  
+                                }else{
+                                    $active  = "";
+                                }
+                                echo '<li class="'.$active.'"><a href="users.php?page='.$i.'">'.$i.'</a></li>';
+                            
+                            }
+                            if($total_page > $page){
+                                echo '<li><a href="users.php?page='.($page + 1).'">Next</a></li>';
+                            }
+                            
+                            echo '</ul>';
+                        }
+                      ?>
+
+
                   <ul class='pagination admin-pagination'>
                       <li class="active"><a>1</a></li>
                       <li><a>2</a></li>
