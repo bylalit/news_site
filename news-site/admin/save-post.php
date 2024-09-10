bba7bcbd5380ef0099d27274513b3d6410cf8876
Lalit0101 <lalitkp0101@gmail.com>
2024-09-10 18:49:57 +0530

    okk

--- news-site/admin/save-post.php
+++ news-site/admin/save-post.php
@@ -9,7 +9,8 @@ if(isset($_FILES["fileToUpload"])){
     $file_size = $_FILES['fileToUpload']['size'];
     $file_tmp = $_FILES['fileToUpload']['tmp_name'];
     $file_type = $_FILES['fileToUpload']['type'];
-    $file_ext = strtolower(end(explode('.', $file_name)));
+    // $file_ext = end(explode(".", $file_name));
+    $file_ext = array_pop(explode(".", $file_name));
     $extensions = array("jpeg", "jpg", "png");
 
     if(in_array($file_ext, $extensions) === false){
@@ -26,12 +27,24 @@ if(isset($_FILES["fileToUpload"])){
     }
 }
 
+session_start();
 
-$tital = mysqli_real_escape_string($conn, $_POST['post_tital']);
+$tital = mysqli_real_escape_string($conn, $_POST['post_title']);
 $description = mysqli_real_escape_string($conn, $_POST['postdesc']);
 $category = mysqli_real_escape_string($conn, $_POST['category']);
 $date = date("d M, Y");
 $author = $_SESSION['user_id'];
 
 
-$sql = "INSERT INTO post(title, description, category,          post_date, authore, post_img) VALUES ('{$tital}','{$description}','{$category}','{$date}','{$author}','{$file_name}')";
+$sql =  "INSERT INTO post(title, description, category, post_date, author, post_img) VALUES ('$tital','$description','$category','$date','$author','$file_name');";
+
+// $sql .= "UPDATE category SET post = post + 1 WHERE category_id = {$category}";
+
+if(mysqli_query($conn, $sql)){
+    header("location: {$hostname}/admin/post.php");
+}
+else{
+    echo "<div class='alert alert-danger'>Query Failed.</div>";
+}
+
+?>
