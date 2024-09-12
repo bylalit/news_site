d76e3a7ed0c227af5fe275ed9070e4f33d6533bb
Lalit0101 <lalitkp0101@gmail.com>
2024-09-12 18:15:28 +0530

    okk done the

--- news-site/admin/delete-post.php
+++ news-site/admin/delete-post.php
@@ -3,7 +3,14 @@
 include "config.php";
 
 $post_id = $_GET['id'];
-$cat_id =  $_GET['cat_id'];
+$cat_id =  $_GET['catid'];
+
+$sql1 = "SELECT * FROM post WHERE post_id = {$post_id};";
+$result = mysqli_query($conn, $sql1) or die("Query Failed");
+$row =  mysqli_fetch_assoc($result);
+
+unlink("upload/".$row['post_img']);
+
 
 $sql = "DELETE FROM post WHERE post_id = {$post_id};";
 $result = mysqli_query($conn, $sql);
