565dbdb9ea9fbc92c816acfc4a11ecf27d65f76d
Lalit0101 <lalitkp0101@gmail.com>
2024-09-12 18:54:36 +0530

    done the

---
+++ news-site/config.php
@@ -0,0 +1,8 @@
+<?php
+
+$hostname = "http://localhost/news/news_site/news-site";
+
+$conn = mysqli_connect('localhost','root','','news-site') or die('Connection failed : ' . mysqli_connect_error());
+
+
+?>
