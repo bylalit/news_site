710b4136602752b0b813c9c174dd695f29afc9d8
Lalit0101 <lalitkp0101@gmail.com>
2024-09-13 23:03:26 +0530

    configfile

--- news-site/config.php
+++ news-site/config.php
@@ -2,7 +2,7 @@
 
 $hostname = "http://localhost/news/news_site/news-site";
 
-$conn = mysqli_connect('localhost','root','','news-site') or die('Connection failed : ' . mysqli_connect_error());
+// $conn = mysqli_connect('localhost','root','','news-site') or die('Connection failed : ' . mysqli_connect_error());
 
 
 ?>
