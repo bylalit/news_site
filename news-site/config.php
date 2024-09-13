eb3ac93dcd44f690d3145ec68afadf73dd81c56c
Lalit0101 <lalitkp0101@gmail.com>
2024-09-13 23:00:00 +0530

    qury fai;ed

--- news-site/config.php
+++ news-site/config.php
@@ -1,8 +1,8 @@
-<!-- <?php
+<?php
 
 $hostname = "http://localhost/news/news_site/news-site";
 
-$conn = mysqli_connect('localhost','root','','news-site') or die('Connection failed : ' . mysqli_connect_error());
+// $conn = mysqli_connect('localhost','root','','news-site') or die('Connection failed : ' . mysqli_connect_error());
 
 
-?> -->
+?>
