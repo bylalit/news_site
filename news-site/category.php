ca811784c650b82303c4de56b0290f1084ddc36d
Lalit0101 <lalitkp0101@gmail.com>
2024-09-15 18:48:36 +0530

    done the

--- news-site/category.php
+++ news-site/category.php
@@ -9,6 +9,10 @@
                   <?php
                             include "config.php";
 
+                            if(isset($_GET['cid'])){
+                                $cat_id = $_GET['cid'];
+                            }
+
                             $limit = 3;
                     
                             if(isset($_GET['page'])){
@@ -21,6 +25,7 @@
                             $sql = "SELECT post.post_id, post.title, post.description, post.post_date, category.category_name, user.username,post.category,post.post_img FROM post
                             LEFT JOIN category ON post.category = category.category_id
                             LEFT JOIN user ON post.author = user.user_id
+                            WHERE post.category = {$cat_id}
                             ORDER BY post.post_id DESC LIMIT {$offset}, {$limit}";
 
                             $result = mysqli_query($conn, $sql) or die("Query Unsuccessful.");
@@ -67,18 +72,20 @@
 
 
                     <?php
-                        $sql1 = "SELECT * FROM post";
+                        $sql1 = "SELECT post FROM category WHERE category_id = {$cat_id}";
                         $result1 = mysqli_query($conn, $sql1) or die("Query Failed");
 
+                        $row = mysqli_fetch_assoc($result1);
+
                         if(mysqli_num_rows($result1) > 0){
-                            $total_records = mysqli_num_rows($result1);
+                            $total_records = $row['post'];
                             
                             $total_page = ceil($total_records / $limit);
 
 
                             echo "<ul class='pagination admin-pagination'>";
                             if($page > 1){
-                                echo  '<li><a href="index.php?page='.($page - 1).'">Prev</a></li>';
+                                echo  '<li><a href="index.php?cid='.$cat_id.'&page='.($page - 1).'">Prev</a></li>';
                             }
                             
                             for($i = 1; $i <=  $total_page; $i++){
@@ -87,11 +94,11 @@
                                 }else{
                                     $active  = "";
                                 }
-                                echo '<li class="'.$active.'"><a href="index.php?page='.$i.'">'.$i.'</a></li>';
+                                echo '<li class="'.$active.'"><a href="index.php?cid='.$cat_id.'&page='.$i.'">'.$i.'</a></li>';
                             
                             }
                             if($total_page > $page){
-                                echo '<li><a href="index.php?page='.($page + 1).'">Next</a></li>';
+                                echo '<li><a href="index.php?cid='.$cat_id.'&page='.($page + 1).'">Next</a></li>';
                             }
                             
                             echo '</ul>';
--- news-site/header.php
+++ news-site/header.php
@@ -50,6 +50,7 @@
                         $active = "";
                 ?>
                 <ul class='menu'>
+                <li><a href='<?php echo $hostname ?>'>Home</a></li>
                     <?php while($row = mysqli_fetch_assoc($result)){
                         if(isset($_GET['cid'])){
                             if($row['category_id'] ==  $cat_id){
